dbc399c2f213b5e8b13655a19ab592e4b2f969dc
axhero7 <axhero7@gmail.com>
2024-06-06 18:29:13 -0700

    Update validation.py

--- validation.py
+++ validation.py
@@ -2,6 +2,7 @@ from datasets import load_dataset
 import torch
 import os
 from transformers import AutoFeatureExtractor
+from datasets import load_dataset, ClassLabel, load_from_disk
 # from main_cuda_machine import vectorized_dataset
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
