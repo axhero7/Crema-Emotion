b777e9af9735e5bfd9a6e600b838d1b849166ada
axhero7 <axhero7@gmail.com>
2024-06-06 18:28:07 -0700

    Update validation.py

--- validation.py
+++ validation.py
@@ -1,6 +1,6 @@
 from datasets import load_dataset
 import torch
-from main_cuda_machine import vectorized_dataset
+# from main_cuda_machine import vectorized_dataset
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
