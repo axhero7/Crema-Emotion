bc978910c1de8f982cf6cad0a55d3956a8f7daf1
Rohan Aggarwal <100031155@mvla.net>
2025-01-03 13:59:22 -0800

    added a cnn

---
+++ cnn_class/cremanet.py
@@ -0,0 +1,73 @@
+from torch import nn
+import torch
+from torchsummary import summary
+
+class CNN_Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=1,
+                out_channels=16,
+                kernel_size=3,
+                stride=1,
+                padding=2
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=16,
+                out_channels=32,
+                kernel_size=3,
+                stride=1,
+                padding=2
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=32,
+                out_channels=64,
+                kernel_size=3,
+                stride=1,
+                padding=2
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=64,
+                out_channels=128,
+                kernel_size=3,
+                stride=1,
+                padding=2
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=128,
+                out_channels=128,
+                kernel_size=3,
+                stride=1,
+                padding=2
+            ),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.flatten = nn.Flatten(start_dim=1)
+        self.linear_1 = nn.Linear(128*3*5, 20)
+        self.linear_2 = nn.Linear(20, 6)
+        self.softmax = nn.Softmax(dim=1)
+    
+    def forward(self, x):
+        x = self.conv5(self.conv4(self.conv3(self.conv2(self.conv1(x)))))
+        x = x.view(-1, 128*3*5)
+        x = self.linear_2(self.linear_1(x))
+        return x
+    
